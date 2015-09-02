d955fef3dc3f498f98022a8617dd513bd713bce6
amarkpark <amarkpark@gmail.com>
2015-09-02 12:52:47 +0000

    Initial commit for FHP Quizes, Quiz 1. ~AMP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quizes::Application.config.secret_key_base = '2324e2fc1e3b771a81bdf2ae907f4c743318b4b316c24bdd0aab1ec86da129fca8db82aa8a8f16cce26e57f8901b0a7c4473f4677c6382e94568ddf5be83e6cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quizes::Application.config.session_store :cookie_store, key: '_quizes_session'
