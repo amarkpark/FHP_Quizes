31ee4660c3ff0c19308c33d4c4c3f4a65c0dbcda
amarkpark <amarkpark@gmail.com>
2015-09-10 04:25:19 +0000

    stupid undefined method error making my life hell with submission form on same page as display data. about to move it to footer of app.html.erb. ~AMP

--- app/controllers/quizes_controller.rb
+++ app/controllers/quizes_controller.rb
@@ -6,5 +6,21 @@ class QuizesController < ApplicationController
 	end
 
 	def quiz2
+		@comments = Comment.all
+	end
+
+	#def new
+	#	@comment = Comment.new
+	#end
+
+	def create
+		Comment.create(comment_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:data, :author, :email)
 	end
 end
